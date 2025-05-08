50e428f4827453cdda6c1b2ad3dde9d7e9da9b50
eduuuu <everton.edu.2009@gmail.com>
2025-05-08 00:24:53 -0300

    Update bruxo.ts

--- src/TrabalhoAvaliativo-A/bruxo.ts
+++ src/TrabalhoAvaliativo-A/bruxo.ts
@@ -1,36 +1,41 @@
+import { Feitico, feiticos, feitico1, feitico2 } from "./Feitiço";
 import { criaturasMagicas } from "./criaturasMagicas";
 
 export class bruxo extends criaturasMagicas{
    varinha:string
    casa:string
-   habilidades:string[]
+   habilidades:Feitico[]
    poderMagico:number
-   constructor(nomeConst:string,tipoConst:string,poderConst:number,varinhaConst:string,casaConst:string,habilidadesConst:string[],poderMagicoConst:number){
-    super(nomeConst,tipoConst,poderConst)
+   constructor(nomeConst:string,tipoConst:string,poderConst:number,HPRecebido:number,varinhaConst:string,casaConst:string,habilidadesConst:Feitico[],poderMagicoConst:number){
+    super(nomeConst,tipoConst,poderConst,HPRecebido)
     this.varinha = varinhaConst;
     this.casa = casaConst;
-    this.habilidades = habilidadesConst;    
+    this.habilidades = habilidadesConst
     this.poderMagico = poderMagicoConst;
    }
-   usarMagia() {
-    const magiaAleatoria1 = Math.floor(Math.random() * this.habilidades.length)
-    const magiaSelecionada1 = this.habilidades[magiaAleatoria1]
-    const poder = Math.floor(Math.random() * 1000);
-    console.log(`${this.nome} usou magia ${this.habilidades[magiaAleatoria1]} e deu ${poder} de dano ao adversário.`);
-    return poder;
+   usarMagia(feiticoRecebido:Feitico):number {
+
+    console.log(`${this.nome} usou magia ${feiticoRecebido.getName()} e deu ${this.poderMagico+feiticoRecebido.getPoderBase()} de dano ao adversário.`);
+    
+    return this.poderMagico+feiticoRecebido.getPoderBase();
   
     }
-    lançarFeitiço():void{
-        const magiaAleatoria2 = Math.floor(Math.random() * this.habilidades.length)
-        const magiaSelecionada2 = this.habilidades[magiaAleatoria2]
-        console.log(`O bruxo ${this.nome} irá lançar a habilidade ${this.habilidades[magiaAleatoria2]} e o poder de magico de ${this.nome} é de ${this.poderMagico}.`)
+    lançarFeitiço():number{
+        const magiaAleatoria1 = Math.floor(Math.random() * this.habilidades.length)
+        const magiaSelecionada1 = this.habilidades[magiaAleatoria1]
+        return this.usarMagia(magiaSelecionada1)
+        
     
+    }
+    getHabilidades():Feitico[]{
+        return this.habilidades
     }
 }
 
-let newBruxo1 = new bruxo("Harry Potter","Humano",100,"Varinha de azevinho com núcleo de pena de fênix"," Gryffindor",["Expecto Patronum","Expelliarmus","Lumos"],1000)
-let newBruxo2 = new bruxo("Hermione Granger","Humano",95,"Varinha de videira com núcleo de corda de coração de dragão"," Gryffindor",["Alohomora", "Expelliarmus", "Petrificus Totalus", "Wingardium Leviosa", "Obliviate", "Protego"],950)
-newBruxo1.usarMagia()
+let newBruxo1 = new bruxo("Harry Potter","Humano",100,60,"Varinha de azevinho com núcleo de pena de fênix"," Gryffindor", [feitico1,feitico2],1000)
+let newBruxo2 = new bruxo("Hermione Granger","Humano",95,50,"Varinha de videira com núcleo de corda de coração de dragão"," Gryffindor",[feitico1,feitico2],950)
+newBruxo1.usarMagia(Feitico.getTudo()[0])
 newBruxo1.lançarFeitiço()
-newBruxo2.usarMagia()
+newBruxo2.usarMagia(Feitico.getTudo()[0])
 newBruxo2.lançarFeitiço()
+newBruxo1.getHabilidades()
