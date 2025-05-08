8241b7fc91dcb6a84f6c8cc1d197a8e7bb65116e
eduuuu <everton.edu.2009@gmail.com>
2025-05-08 00:22:51 -0300

    Update myGuadion.ts

--- src/TrabalhoAvaliativo-A/myGuadion.ts
+++ src/TrabalhoAvaliativo-A/myGuadion.ts
@@ -1,5 +1,32 @@
-import { criaturasMagicas } from "./criaturasMagicas";
+import { criaturasMagicas } from "./criaturasMagicas"
 
-export abstract class guardiao extends criaturasMagicas{
+export abstract class Guardiao extends criaturasMagicas{
    abstract protegerSegredo():void
+   
+  }
+
+export class Tipos extends Guardiao{
+   segredo:string
+
+   constructor(nomeRecebido:string,tipoRecebido:string,poderRecebido:number,HPRecebido:number,segredoGuardado:string){
+      super(nomeRecebido,tipoRecebido,poderRecebido,HPRecebido)
+      this.segredo = segredoGuardado
+     
+   }
+
+   protegerSegredo(): void {
+      console.log(`O nome do guardiao é ${this.nome} tipo é ${this.tipo} e ele irá proteger ${this.segredo}`)
+   }
+   usarMagia(): number{
+      console.log(`${this.nome} usou veneno letal. `)
+      return this.poder
+   }
+   
+
+   
 }
+
+export let myGuardian = new Tipos("Basilisk","Subterrâneo",2000,2000,"Camarã Secreta")
+
+myGuardian.protegerSegredo()
+myGuardian.usarMagia()
